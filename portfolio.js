f1113037e4ab99c2415dc89950f7434281a60762
Andreea Uta <dreea.9965@gmail.com>
2017-03-16 16:08:05 -0400

    portfolio 2

--- portfolio.js
+++ portfolio.js
@@ -1,5 +1,16 @@
-$(document).ready(function(){
-	$("#demosMenu").change(function(){
-	  window.location.href = $(this).find("option:selected").attr("id") + '.html';
-	});
+$(doctype) (function(){}
+
+$.fn.fullpage.moveSectionUp();
+$.fn.fullpage.moveSectionDown();
+$.fn.fullpage.moveTo(section, slide);
+$.fn.fullpage.moveSlideRight();
+$.fn.fullpage.moveSlideLeft();
+$.fn.fullpage.setAutoScrolling(boolean);
+$.fn.fullpage.setAllowScrolling(boolean);
+$.fn.fullpage.setKeyboardScrolling(boolean);
+$.fn.fullpage.setScrollingSpeed(700);
+$.fn.fullpage.destroy(type);
+// (type = all or empty);
+$.fn.fullpage.reBuild();
+
 });
